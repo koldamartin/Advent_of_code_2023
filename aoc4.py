f634d80f3c3852015029b17318311fd948f0dd0a
Martin Kolda <68967537+koldamartin@users.noreply.github.com>
2023-12-05 12:54:27 +0100

    Added solution to part 2

--- aoc4.py
+++ aoc4.py
@@ -2,20 +2,31 @@ with open('aoc_4.txt') as f:
   contents = f.readlines()
 
 result_sum = 0
+scores = [1 for z in range(len(contents))]
 
-for line in contents:
-    winning_numbers = list(filter(None, line.split(":")[1].split("|")[0].strip(" ").split(" ")))
-    my_card_numbers = list(filter(None, line.split("|")[1].strip(" ").replace("\n","").split(" ")))
+for x in range(len(contents)):
+    winning_numbers = list(filter(None, contents[x].split(":")[1].split("|")[0].strip(" ").split(" ")))
+    my_card_numbers = list(filter(None, contents[x].split("|")[1].strip(" ").replace("\n","").split(" ")))
 
     points = 0
+    winning_cards = 0
     first = True
-    for number in winning_numbers:
-        if number in my_card_numbers and first:
+    
+    for i in range(len(winning_numbers)):
+        if winning_numbers[i] in my_card_numbers and first:
             points += 1
+            winning_cards += 1
             first = False
-        elif number in my_card_numbers:
+        elif winning_numbers[i] in my_card_numbers:
             points *= 2
-    
+            winning_cards +=1
+
     result_sum += points    
-    
-print(f'The result is {result_sum}')
+
+    # Solution to part 2:
+    for y in range(scores[x]):
+        for j in range(winning_cards):
+            scores[x+j+1] += 1
+        
+print(f'The solution to part 1 is {result_sum}')
+print(f'The solution to part 2 is {sum(scores)}')
